fe9654f4af284716feae9a96368675f1a1a5354e
Nkennyelvis <e.nsengiman@alustudent.com>
2025-07-21 15:02:03 +0200

    Api

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/python3
 """Script to get todos for a user from API"""
 
@@ -6,22 +7,22 @@ import sys
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
+        print("Usage: python3 0-gather_data_from_an_API.py <Employee_id>")
         sys.exit(1)
 
     try:
-        employee_id = int(sys.argv[1])
+        Employee_id = int(sys.argv[1])
     except ValueError:
         print("Employee ID must be an integer")
         sys.exit(1)
 
     # API endpoints
     user_url = (
-        "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
+        "https://jsonplaceholder.typicode.com/users/{}".format(Employee_id)
     )
     todos_url = (
         "https://jsonplaceholder.typicode.com/todos?userId={}"
-        .format(employee_id)
+        .format(Employee_id)
     )
 
     # Get employee data
@@ -30,7 +31,7 @@ if __name__ == "__main__":
         print("Employee not found")
         sys.exit(1)
 
-    employee_name = user_response.json().get("name")
+    Employee_name = user_response.json().get("name")
 
     # Get TODOs
     todos_response = requests.get(todos_url)
@@ -41,7 +42,7 @@ if __name__ == "__main__":
 
     print(
         "Employee {} is done with tasks({}/{}):".format(
-            employee_name, len(done_tasks), len(todos)
+            Employee_name, len(done_tasks), len(todos)
         )
     )
     for task in done_tasks:
